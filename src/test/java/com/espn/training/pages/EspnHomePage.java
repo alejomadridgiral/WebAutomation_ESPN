55688b2d257f277b19c33438e816c05c31ac0d79
Alejo Madrid <alejandro.madrid@globant.com>
2021-11-07 09:27:07 -0500

    Sign Up fill fields

--- src/test/java/com/espn/training/pages/EspnHomePage.java
+++ src/test/java/com/espn/training/pages/EspnHomePage.java
@@ -25,9 +25,6 @@ public class EspnHomePage extends BasePage {
     @FindBy(css=".btn.btn-secondary.ng-isolate-scope")
     private WebElement signUp;
 
-    @FindBy(css="#did-ui-view > div > section > section > form > section.section.section-your-account-fields > div:nth-child(1) > div.field.field-first-name.ng-scope.ng-isolate-scope.badgeable.field-required.field-error > label > span.input-wrapper.ng-scope > input")
-    private WebElement firstName;
-
     public void triggerUser(){
         userTrigger.click();
     }
@@ -40,4 +37,50 @@ public class EspnHomePage extends BasePage {
         getDriver().switchTo().frame("disneyid-iframe");
         signUp.click();
     }
+
+    @FindBy(css="input[placeholder='First Name']")
+    private WebElement firstName;
+
+    public void fillFirstName(String fName){
+        firstName.click();
+        firstName.sendKeys(fName);
+    }
+
+    @FindBy(css="input[placeholder='Last Name']")
+    private WebElement lastName;
+
+    public void fillLastName(String flastName){
+        lastName.click();
+        lastName.sendKeys(flastName);
+    }
+
+    @FindBy(css="input[placeholder='Email Address']")
+    private WebElement emailAddress;
+
+    public void fillEmail(String fEmail){
+        emailAddress.click();
+        emailAddress.sendKeys(fEmail);
+    }
+
+    @FindBy(css="input[placeholder='Password (case sensitive)']")
+    private WebElement password;
+
+    public void fillPassword(String fPassword){
+        password.click();
+        password.sendKeys(fPassword);
+    }
+
+
+
+
+
+
+
+    @FindBy(className = "display-user")
+    private WebElement userName;
+
+    public String getUserName(){
+        return userName.getText();
+    }
+
 }
--- src/test/java/com/espn/training/test/EspnTest.java
+++ src/test/java/com/espn/training/test/EspnTest.java
@@ -1,15 +1,31 @@
 package com.espn.training.test;
 
 import com.espn.training.pages.EspnHomePage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class EspnTest extends BaseTest{
 
-    @Test
-    public void testEspnLogin() {
+    @BeforeTest
+    public void singUp(){
         EspnHomePage home = getEspnHomePage();
         home.triggerUser();
         home.logIn();
         home.signUp();
+        home.fillFirstName("Alejandro");
+        home.fillLastName("Madrid");
+        home.fillEmail("alejandro.madrid2@globant.com");
+        home.fillPassword("GLo_ESPN_1*#");
+//        home.signUp();
     }
+
+    @Test(groups="signUp")
+    public void testEspnLogin() {
+////        EspnHomePage home = getEspnHomePage();
+////        home.triggerUser();
+//        Assert.assertEquals(home.getUserName(), "Alejandro!");
+    }
+
+
 }
