42e747039532f22f14f9f1bbcc16feebe24aba39
Alejo Madrid <alejandro.madrid@globant.com>
2021-11-08 00:13:34 -0500

    Last funtional test before cleaning

--- src/test/java/com/espn/training/test/EspnTest.java
+++ src/test/java/com/espn/training/test/EspnTest.java
@@ -8,32 +8,34 @@ import org.testng.annotations.Test;
 
 public class EspnTest extends BaseTest{
 
-//    @BeforeTest (groups = "Signup")
-//    public void singUp(){
-//        EspnHomePage home = getEspnHomePage();
-//        home.triggerUser();
-//        home.logIn();
-//        home.switchToLogInIframe();
-//        home.signUp();
-//        home.fillFirstName("Alejandro");
-//        home.fillLastName("Madrid");
-//        home.fillEmail("alejandro.madrid4@globant.com");
-//        home.fillPassword("GLo_ESPN_1*#");
-//        home.fullfillSingUpSubmit();
-//    }
-
+    @BeforeTest(groups="Signup")
+    public void singUp(){
+        EspnHomePage home = getEspnHomePage();
+        home.triggerUser();
+        home.logIn();
+        home.switchToLogInIframe();
+        home.signUp();
+        home.fillFirstName("Alejandro");
+        home.fillLastName("Madrid");
+        home.fillEmail("alejandro.madrid6@globant.com");
+        home.fillPassword("GLo_ESPN_1*#");
+        home.fullfillSingUpSubmit();
+        home.clickLogOut();
+    }
 
 //    @BeforeTest (groups = "LogIn")
-//    public void login(){
-//        EspnHomePage home = getEspnHomePage();
-//        home.triggerUser();
-//        home.logIn();
-//        home.switchToLogInIframe();
-//        home.fillEmailLogIn("alejandro.madrid4@globant.com");
-//        home.fillPasswordLogIn("GLo_ESPN_1*#");
-//        home.clickLogIn();
-//    }
-
+    @Test(groups="logIn")
+    public void login(){
+        EspnHomePage home = getEspnHomePage();
+        home.triggerUser();
+        home.logIn();
+        home.switchToLogInIframe();
+        home.fillEmailLogIn("alejandro.madrid1@globant.com");
+        home.fillPasswordLogIn("GLo_ESPN_1*#");
+        home.clickLogIn();
+        home.triggerUser();
+        Assert.assertEquals(home.getUserName(), "WelcomeAlejandro!");
+    }
 
 //    @Test (groups="logIn, Signup", priority = 0)
 //    public void testEspnLogin() {
@@ -42,25 +44,44 @@ public class EspnTest extends BaseTest{
 //        Assert.assertEquals(home.getUserName(), "WelcomeAlejandro!");
 //    }
 
-//    @Test (priority = 1)
-//    public void testEspnLogOut(){
-//        EspnHomePage home = getEspnHomePage();
-//        home.clickLogOut();
-//        Assert.assertEquals(home.getUserNameLogOut(), "Customize ESPN");
-//    }
+    @Test(groups="LogOut")
+    public void testEspnLogOut(){
+        EspnHomePage home = getEspnHomePage();
+        home.triggerUser();
+        home.logIn();
+        home.switchToLogInIframe();
+        home.fillEmailLogIn("alejandro.madrid1@globant.com");
+        home.fillPasswordLogIn("GLo_ESPN_1*#");
+        home.clickLogIn();
+        home.triggerUser();
+        home.clickLogOut();
+        Assert.assertEquals(home.getUserNameLogOut(), "Customize ESPN");
+    }
 
-//    @Test
-//    public void cancelAccount(){
-//        EspnHomePage home = getEspnHomePage();
-//        home.triggerUser();
-//        home.clickEspnProfile();
-//        home.switchToLogInIframe();
-//        home.clickCancelAccount();
-//        home.clickConfirmAccountDelete();
-//        Assert.assertEquals(home.getAccountDeactivate(), "Customize ESPN");
-//    }
+    @Test(groups="CancelAccount")
+    public void cancelAccount(){
+        EspnHomePage home = getEspnHomePage();
+        home.triggerUser();
+        home.logIn();
+        home.switchToLogInIframe();
+        home.fillEmailLogIn("alejandro.madrid3@globant.com");
+        home.fillPasswordLogIn("GLo_ESPN_1*#");
+        home.clickLogIn();
+        home.triggerUser();
+        home.clickEspnProfile();
+        home.switchToLogInIframe();
+        home.clickCancelAccount();
+        home.clickConfirmAccountDelete();
+        home.triggerUser();
+        home.logIn();
+        home.switchToLogInIframe();
+        home.fillEmailLogIn("alejandro.madrid3@globant.com");
+        home.fillPasswordLogIn("GLo_ESPN_1*#");
+        home.clickLogIn();
+        Assert.assertEquals(home.getAccountDeactivate(), "Account Deactivated");
+    }
 
-    @Test
+    @Test(groups="CancelAccount")
     public void testCancelAccount(){
         EspnHomePage home = getEspnHomePage();
         home.triggerUser();
@@ -71,7 +92,4 @@ public class EspnTest extends BaseTest{
         home.clickLogIn();
         Assert.assertEquals(home.getAccountDeactivate(), "Account Deactivated");
     }
-
-
-
 }
